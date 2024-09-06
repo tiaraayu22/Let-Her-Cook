a5ec3c1da710e38e23e09f600043ef707d0ce200
tiaraayu22 <tiarayup22@gmail.com>
2024-09-06 11:48:15 +0700

    Add files via upload

---
+++ lpage.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Let Him Cook</title>
+	<link rel="stylesheet" type="text/css" href="css/lpage.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" rel="stylesheet">
+</head>
+
+<body style="overflow-y: scroll;">
+	<header>
+		<div class="container">
+			<nav class="navbar">
+				<a href="#" class="nav-logo"><img src="../proyek2_web/assets/img/logo.png" alt="Let_Him_Cook" width="100%" height="100%" class="d-inline-block align-text-top"></a>
+				<ul class="nav-menu">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="#About_us">About Us</a></li>
+					<li><a href="#kontak">Contact Us</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	<section class="about-section">
+		<div id="About_us" class="container">
+			<div class="about-grid">
+				<div class="about-item">
+					<h2>About Us</h2>
+					<p>Let Him Cook adalah rumah bagi puluhan resep autentik makanan Indonesia yang menggugah selera dan membangkitkan nostalgia. Kami adalah komunitas yang bersatu dalam kecintaan kami terhadap masakan tradisional Indonesia, dan kami bangga menjadi wadah di mana warisan kuliner kami dapat diabadikan dan dinikmati oleh generasi masa kini dan mendatang.</p>
+					<p>Dengan semangat untuk mempertahankan cita rasa khas Indonesia, tujuan kami adalah mempertahankan esensi dari cita rasa khas Indonesia. Let Him Cook menawarkan resep-resep yang telah teruji dan teruji coba, juga menjadi sumber inspirasi bagi para pecinta masakan untuk menjelajahi kekayaan kuliner nusantara.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<section class="contact-section">
+		<div id="kontak" class="container">
+			<h2>Contact Us</h2>
+			<p>Untuk informasi lebih lanjut atau jika Anda memiliki pertanyaan atau saran, silakan hubungi kami di:</p>
+			<ul></ul>
+			<li><a href="https://wa.link/9rgwty" target="_blank" alt="Chat via WhatsApp"><span class="fas fa-phone"></span> 085244142201</a></li>
+			<li><a href="mailto:lethimcookservice@gmail.com" target="_blank"> <span class="fas fa-envelope"></span> lethimcookservice@gmail.com</a></li>
+			<li><a href="https://maps.app.goo.gl/8Z16DZ5SQxh1CLeQ9" target="_blank"> <span class="fas fa-map-marker-alt"></span>Jl. Komjen.Pol.M.Jasin No.9, Kota Depok, Jawa Barat 16451</a></li>
+			</ul>
+		</div>
+	</section>
+
+	<footer>
+		<div class="container">
+			<p>&copy; 2023 Let Him Cook. All rights reserved.</p>
+		</div>
+	</footer>
+</body>
+
+</html>
